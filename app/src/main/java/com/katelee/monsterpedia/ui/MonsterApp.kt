c9554076f230bf9d4107b10ff423067ee2293531
Kate Lee <xoolwater@gmail.com>
2025-12-06 17:13:06 +0800

    詳細頁面加上返回按鈕

--- app/src/main/java/com/katelee/monsterpedia/ui/MonsterApp.kt
+++ app/src/main/java/com/katelee/monsterpedia/ui/MonsterApp.kt
@@ -1,7 +1,11 @@
 package com.katelee.monsterpedia.ui
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -24,8 +28,22 @@ fun MonsterApp() {
 
     Scaffold(
         topBar = {
-            if (currentRoute == Routes.LIST) {
-                TopAppBar(title = { Text(stringResource(R.string.app_name)) })
+            when(currentRoute) {
+                Routes.LIST ->
+                    TopAppBar(title = { Text(stringResource(R.string.app_name)) })
+                Routes.DETAIL -> {
+                    val id = navController.currentBackStackEntryFlow
+                        .collectAsState(initial = navController.currentBackStackEntry).value?.arguments?.getString("id")
+                    TopAppBar(
+                        title = { Text(stringResource(R.string.app_name)) },
+                        navigationIcon = {
+                            IconButton(onClick = { navController.popBackStack() }) {
+                                Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
+                            }
+                        },
+                        actions = { Text("#${id ?: ""}") }
+                    )
+                }
             }
         }
     ) { padding ->
