571af5515662850550f4c575a64c276142f1ab5e
Kate Lee <xoolwater@gmail.com>
2025-12-10 12:56:26 +0800

    詳情加上垂直滾動

--- feature/encyclopedia/src/main/java/com/katelee/monsterpedia/feature/encyclopedia/ui/MonsterDetailScreen.kt
+++ feature/encyclopedia/src/main/java/com/katelee/monsterpedia/feature/encyclopedia/ui/MonsterDetailScreen.kt
@@ -16,7 +16,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Card
@@ -62,7 +64,9 @@ fun MonsterDetailScreen(
             ErrorScreen(error = it)
         }
         state.item?.let { monster ->
-            Column(modifier = Modifier.background(Color.Black).fillMaxHeight()) {
+            Column(modifier = Modifier.background(Color.Black)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())) {
                 var dominantColor by remember { mutableStateOf(Color.Gray) }
                 Card(modifier = Modifier
                         .fillMaxWidth(),
